9a6d27a9445257ff20f690dcfb2d4317c418f5c9
daiwata <watanabedai5427@gmail.com>
2023-12-30 11:27:20 +0900

    フォルダを再帰的に処理するよう修正、excludeオプションの追加

--- xml2Html.py
+++ xml2Html.py
@@ -3,6 +3,9 @@ import sys
 import json
 import xmltodict
 from pathlib import Path
+import argparse
+import fnmatch
+import glob
 
 """
 xml2html.py
@@ -41,6 +44,9 @@ th {
 td.undefined{
     background-color: #eeeeee;
 }
+ul {
+    margin:0 0 0 -20px;
+}
 
 </style>
 </head>
@@ -50,57 +56,114 @@ td.undefined{
 </html>
 """
 
-
 """
 Converts a JSON or YAML object into an HTML string.
 """
+
+
 def obj2html(obj):
     """
-    Converts a JSON or YAML object into an HTML string with stable ordering.
+    Converts a JSON or YAML object into an HTML string.
+    Preserves the order of keys as they appear in the object.
     Adds a class 'undefined' to <td> elements if the key does not exist in the dict.
     """
     if isinstance(obj, dict):
-        html = '<table><tbody>'
-        for key in sorted(obj.keys()):
-            html += f'<tr><th>{key}</th><td>{obj2html(obj[key])}</td></tr>'
-        html += '</tbody></table>'
+        html = "<table><tbody>"
+        for key, value in obj.items():
+            html += f"<tr><th>{key}</th><td>{obj2html(value)}</td></tr>"
+        html += "</tbody></table>"
     elif isinstance(obj, list):
         if all(isinstance(item, dict) for item in obj):
-            keys = sorted({key for item in obj for key in item.keys()})
-            html = '<table><tbody><tr>' + ''.join(f'<th>{key}</th>' for key in keys) + '</tr>'
+            # Collect all unique keys in the order they appear
+            keys = []
             for item in obj:
-                html += '<tr>'
+                for key in item.keys():
+                    if key not in keys:
+                        keys.append(key)
+
+            html = (
+                "<table><tbody><tr>"
+                + "".join(f"<th>{key}</th>" for key in keys)
+                + "</tr>"
+            )
+            for item in obj:
+                html += "<tr>"
                 for key in keys:
                     value = item.get(key, "")
                     class_attr = ' class="undefined"' if value == "" else ""
-                    html += f'<td{class_attr}>{obj2html(value)}</td>'
-                html += '</tr>'
-            html += '</tbody></table>'
+                    html += f"<td{class_attr}>{obj2html(value)}</td>"
+                html += "</tr>"
+            html += "</tbody></table>"
         else:
-            html = '<ul>' + ''.join(f'<li>{obj2html(item)}</li>' for item in obj) + '</ul>'
+            html = (
+                "<ul>" + "".join(f"<li>{obj2html(item)}</li>" for item in obj) + "</ul>"
+            )
     else:
-        html = str(obj).replace('\n', '<br>')
+        html = str(obj).replace("\n", "<br>")
     return html
 
-if len(sys.argv) < 2 or (not Path(sys.argv[1]).exists()):
-    print("引数にファイルを渡してください")
-    sys.exit()
 
-input_xml_path = sys.argv[1]
-input_xml_name = os.path.splitext(os.path.basename(input_xml_path))[0]
+def is_excluded(path, exclude_patterns):
+    name = os.path.basename(path)
+    return any(fnmatch.fnmatch(name, pattern) for pattern in exclude_patterns)
 
-with open(input_xml_path, encoding='UTF-8') as f:
-    input_xml_contents = f.read()
-    dict_data = xmltodict.parse(input_xml_contents)
 
-#中間jsonのファイル出力
-#with open(input_xml_name + '.json', 'w',  encoding='UTF-8') as f:
-#    f.write(json.dumps(dict_data))
+def find_xml_files(pattern, exclude_patterns):
+    for file_path in glob.glob(pattern, recursive=True):
+        if is_excluded(file_path, exclude_patterns):
+            continue
+        if os.path.isfile(file_path) and file_path.endswith(".xml"):
+            yield file_path
+        elif os.path.isdir(file_path):
+            for root, dirs, files in os.walk(file_path):
+                dirs[:] = [d for d in dirs if not is_excluded(d, exclude_patterns)]
+                for file in files:
+                    if fnmatch.fnmatch(file, "*.xml") and not is_excluded(
+                        file, exclude_patterns
+                    ):
+                        yield os.path.join(root, file)
 
-html_contents = obj2html(dict_data)
 
-output_html_contents = html_template.replace("!!title!!", input_xml_name).replace("!!contents!!", html_contents)
+def process_xml_file(xml_file_path, base_directory):
+    # Function to process a single XML file
+    with open(xml_file_path, encoding="UTF-8") as f:
+        input_xml_contents = f.read()
+        dict_data = xmltodict.parse(input_xml_contents)
 
-with open(input_xml_name + '.html', 'w',  encoding='UTF-8') as f:
-    f.write(output_html_contents)
+    html_contents = obj2html(dict_data)
+    input_xml_name = os.path.splitext(os.path.basename(xml_file_path))[0]
+    output_html_contents = html_template.replace("!!title!!", input_xml_name).replace(
+        "!!contents!!", html_contents
+    )
 
+    # Creating output file path maintaining the folder structure relative to the base directory
+    relative_path = os.path.relpath(xml_file_path, base_directory)
+    output_file_path = os.path.join(
+        base_directory, os.path.splitext(relative_path)[0] + ".html"
+    )
+
+    # Creating output directory if it does not exist
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+    with open(output_file_path, "w", encoding="UTF-8") as f:
+        f.write(output_html_contents)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="XML to HTML converter")
+    parser.add_argument("pattern", help="Wildcard pattern for XML files or directories")
+    parser.add_argument(
+        "--exclude",
+        help="Comma-separated list of file or folder names to exclude (supports wildcards)",
+        default="",
+    )
+    args = parser.parse_args()
+
+    exclude_patterns = args.exclude.split(",") if args.exclude else []
+    input_pattern = args.pattern
+
+    for xml_file in find_xml_files(input_pattern, exclude_patterns):
+        process_xml_file(xml_file, os.path.dirname(xml_file))
+
+
+if __name__ == "__main__":
+    main()
